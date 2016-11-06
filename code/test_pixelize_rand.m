1b11f699a38ac5c30ac0427706783a8dbbabcabf
Gabriel Peyré <gabriel.peyre@ceremade.dauphine.fr>
2016-11-06 21:23:22 +0100

    Added images

--- code/test_pixelize_rand.m
+++ code/test_pixelize_rand.m
@@ -1,7 +1,7 @@
 %%
 % Generate pixelization.
 
-name = 'ens5';
+% name = 'ens7';
 
 rep = 'results/';
 if not(exist(rep))
@@ -16,8 +16,18 @@ height = 512;
 
 remap = @(t)1.5 * ( (t(1)-1/2)^2 + (t(2)-1/2)^2 );
 switch name
-    case {'paris1' 'paris3'}
-        remap = @(t)1.5 * (t(1)-1/2)^2 + 3*(t(2)-1/2)^2;
+    case 'ens6'
+        remap = @(t)1 * (t(1)-1/2)^2 + 2*(t(2)-1/2)^2;
+    case 'ens6'
+        remap = @(t)1.5 * (t(1)-1/2)^2 + 1.5*(t(2)-.7)^2;
+    case 'ens8'
+        remap = @(t)1.5 * (t(1)-.55)^2 + .3*(t(2)-.8)^2;
+    case 'ens9'
+        remap = @(t)1.3 * (t(1)-.5)^2 + 1.3*(t(2)-.1)^2;
+    case 'ens13'
+        remap = @(t).7 * (t(1)-.5)^2 + 1.5*(t(2)-.5)^2;
+    case 'ens14'
+        remap = @(t).7 * (t(1)-.45)^2 + 1.5*(t(2)-.47)^2;
 end
 
 f = double(imread([name '.jpg']));
